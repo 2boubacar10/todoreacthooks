d9b46d27b3a4e29b22bf9769b1454584fac23083
Boubacar Diallo <fereya1990@gmail.com>
2019-12-12 14:09:01 +0000

    Updates

---
+++ precache-manifest.46dc63146fe8fe6513446ccf6f0bd5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e2cc244a376f624af1ed696def1f1b",
+    "url": "/todoreacthooks/index.html"
+  },
+  {
+    "revision": "98441019f36c4a4b01fb",
+    "url": "/todoreacthooks/static/css/main.d4e64339.chunk.css"
+  },
+  {
+    "revision": "a964349a46fb996d458a",
+    "url": "/todoreacthooks/static/js/2.7df547f4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todoreacthooks/static/js/2.7df547f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98441019f36c4a4b01fb",
+    "url": "/todoreacthooks/static/js/main.73ae3762.chunk.js"
+  },
+  {
+    "revision": "eccc221e639f1ea30e9d",
+    "url": "/todoreacthooks/static/js/runtime-main.b2aadd67.js"
+  }
+]);
